747e01a15abde3aeaf55fac883aae30a1db49718
Mohit Bagadia <bagadiamohith@gmail.com>
2018-02-21 20:08:05 +0530

    e2e process

--- MercuryTours/src/pages/BookAFlightPageFactory.java
+++ MercuryTours/src/pages/BookAFlightPageFactory.java
@@ -3,6 +3,7 @@ package pages;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.Select;
 
 import tests.FlightFinder;
@@ -24,6 +25,7 @@ public class BookAFlightPageFactory {
 	@FindBy (name="cc_mid_name") WebElement creditMiddleNameBox;
 	@FindBy (name="cc_last_name") WebElement creditLastNameBox;
 	@FindBy (css="input[name='buyFlights'][src='/images/forms/purchase.gif']") WebElement securePurchaseButton;
+	@FindBy (xpath="/html/body/div/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr[1]/td[2]/table/tbody/tr[3]/td/p/font/b/font[2]") WebElement conf_message;
 	
 	public void setPassengerDetails(String passFirstName0, String passLastName0, String mealType) {
 		for(int i=0; i<=passCount; i++) {
@@ -33,12 +35,33 @@ public class BookAFlightPageFactory {
 		}
 	}
 	
-	public void bookAFlight() {
-		
+	public void creditCardDetails(String cardType, String creditCardNum, int expMonth, int expYear, String passFirstName0,
+			String passLastName0) {
+		new Select(creditCardSelect).selectByValue(cardType);
+		creditnumberBox.sendKeys(String.valueOf(creditCardNum));
+		new Select(creditExpMonthSelect).selectByIndex(expMonth);
+		new Select(creditExpYearSelect).selectByValue(String.valueOf(expYear));
+		creditFirstNameBox.sendKeys(passFirstName0);
+		creditLastNameBox.sendKeys(passLastName0);
+	}
+	
+	public void submit() {
+		securePurchaseButton.click();
+	}
+	
+	public String confirmationMessage() {
+		return conf_message.getText();
+	}
+	
+	public void bookAFlight(String passFirstName0, String passLastName0, String mealType,String cardType, String creditCardNum, int expMonth, int expYear) {
+		setPassengerDetails(passFirstName0, passLastName0, mealType);
+		creditCardDetails(cardType, creditCardNum, expMonth, expYear, passFirstName0, passLastName0);
+		submit();
 	}
 	
 	public BookAFlightPageFactory(WebDriver driver) {
 		this.driver=driver;
+		PageFactory.initElements(driver, this);
 	}
 
 }
---
+++ MercuryTours/src/tests/BookAFlight.java
@@ -0,0 +1,56 @@
+package tests;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import pages.BookAFlightPageFactory;
+import pages.FlightFinderPageFactory;
+import pages.LoginPageFactory;
+import pages.SelectFlightPageFactory;
+
+public class BookAFlight {
+	
+	WebDriver driver;
+	String webURL = "http://newtours.demoaut.com/";
+	String userName="mohit", password="mohit",browserType = "chrome", tripType="round trip",  
+		departFrom="New York",fromMonth="3",fromDay="19",departTo="Paris",toMonth="3",toDay="25",serviceClass="business",
+		passFirstName0 = "Mohit", passLastName0="Kumar",mealType="HNML",cardType="BA",creditCardNum="4444555566667777";
+	int passCount=0, airline=2,expMonth=4,expYear=2000;
+	
+	@Test
+	public void bookAFlightTest() {
+		System.out.println("Executing the test");
+		BookAFlightPageFactory bookFlight = new BookAFlightPageFactory(driver);
+		bookFlight.bookAFlight(passFirstName0, passLastName0, mealType, cardType, creditCardNum, expMonth, expYear);
+		Assert.assertTrue(bookFlight.confirmationMessage().contains("Your"));
+		System.out.println(bookFlight.confirmationMessage());
+	}
+	
+	@BeforeMethod
+	public void setUp() {
+		System.out.println("Initializing the Test");
+		driver = utilities.DriverFactory.open(browserType);
+		driver.get(webURL);
+		LoginPageFactory loginPage = new LoginPageFactory(driver);
+		loginPage.login(userName, password);
+		Assert.assertTrue(loginPage.confirmation().contains("Flight"));
+		System.out.println("The confirmation message is: "+loginPage.confirmation());
+		FlightFinderPageFactory flightFinder = new FlightFinderPageFactory(driver);
+		flightFinder.flighFinder(tripType, passCount, departFrom, fromMonth, fromDay, departTo, toMonth, toDay, serviceClass, airline);
+		Assert.assertTrue(flightFinder.pageTitle().contains("Select a Flight"));
+		System.out.println("The title of the page is: "+flightFinder.pageTitle());
+		SelectFlightPageFactory selectFlight = new SelectFlightPageFactory(driver);
+		selectFlight.selectFlight();
+		Assert.assertTrue(selectFlight.confirmation().contains("Book a Flight"));
+	}
+	
+	@AfterMethod
+	public void tearDown() {
+		System.out.println("Closing the test");
+		driver.quit();
+	}
+
+}
